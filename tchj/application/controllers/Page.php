878da2cbed81535ccee84a8f0219dd08079e173b
丁鹏飞 <dingpengfei@dingpengfeideMacBook-Pro.local>
2018-06-26 22:37:45 +0800

    checkPower

--- tchj/application/controllers/Page.php
+++ tchj/application/controllers/Page.php
@@ -10,56 +10,6 @@ class Page extends MY_Controller{
 		parent::__construct();
 	}
 	
-	//检查当前操作的权限
-	//$usertype 用户类型常量数组 如果为空则不需要验证
-	//$op 操作参数，用于检测某些特例情况
-	//$arr 检测某些特殊情况可能用到的数据对象
-	private function _chkPower($usertype=[],$op='',$arr=array()){
-		$return=array(
-			'code'=>'',
-			'message'=>''
-		);
-
-		$user = $this->session->userdata('user_info');
-		if (count($usertype)>0){
-			if (in_array($user['tid'],$usertype)==false){
-				$return['code']='403';
-				$return['message']='当前用户没有权限进行该操作';
-				return $return;
-			}
-		}
-		if ($op=='') return $return;
-		$tempArray=explode(',',$op);
-		for ($i=0; $i<count($tempArray); $i++){
-			switch($op){
-				case 'ajax':
-					//判定页面强制ajax访问
-					if (!isset($_SERVER['HTTP_X_REQUESTED_WITH']) || strtolower($_SERVER['HTTP_X_REQUESTED_WITH'])!='xmlhttprequest'){
-						echo '当前页面访问不正确';
-						die();
-					}
-					break;
-				case 'accedit':
-					//检查用户修改操作 非管理员用户只能修改自己的账户信息
-					if ($user['tid']!=USERM && $user['uid']!=$arr['uid']){
-						$return['code']='403';
-						$return['message']='当前账户没有权限修改其他用户的资料';
-						return $return;
-					}
-					break;
-				case 'accstop':
-					//禁用用户 无法禁用自己的账户
-					if ($user['uid']==$arr['uid']){
-						$return['code']='401';
-						$return['message']='无法禁用自己的账户';
-						return $return;
-					}
-					break;
-			}
-		}
-		return $return;
-	}
-	
 	//生成应对ajax的返回对象
 	private function _getReturn($code=0,$message='',$data=''){
 		$return=array('code'=>0,'message'=>'','data'=>'');
